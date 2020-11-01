224f8817e9d5b66f7657addb9cb4afa80c9c7f60
momokahori124@gmail.com <momoka@mbp-2.local>
2020-11-01 11:25:33 +0900

    finish

---
+++ index.js
@@ -0,0 +1,50 @@
+
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () =>{
+    navbarMenu.classList.toggle('is-active');
+})
+
+
+
+// tab menu 
+
+// const tabs = document.querySelectorAll('.tabs li')
+// const tabContentBoxes = document.querySelectorAll('.tab-content  div')
+
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(item => item.classList.remove('is-active'));
+    tab.classList.add('is-active');
+
+    const target = tab.dataset.target;
+    console.log(target);
+    tabContentBoxes.forEach(box => {
+      if (box.getAttribute('id') === target) {
+        box.classList.remove('is-hidden');
+      } else {
+        box.classList.add('is-hidden');
+      }
+    })
+  })
+})
+
+// modal
+
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background')
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+    console.log('------')
+    modal.classList.add('is-active');
+})
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
